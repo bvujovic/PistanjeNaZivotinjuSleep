b3194b04a0de2f78d9894e3135e0a06a4a92e642
bvujovic <bv.net@outlook.com>
2020-01-08 10:12:57 +0100

    Dodat je taster za vol- na buzzer-u. Dodat je kôd koji menja jacinu zvuka preko vol tastera. Dodate su 2 slike test verzije aparata.

--- PistanjeNaZivotinjuSleep.ino
+++ PistanjeNaZivotinjuSleep.ino
@@ -14,19 +14,25 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
-const int pinPIR = 2;          // PIR senzor; ako se promeni ova vrednost, obavezno izmeniti PCINT? u sleep() i setup()
-const int pinVolUp = 1;        // taster za pojacavanje zvuka na buzzer-u
-const int pinBuzz = 0;         // buzzer/zvucnik
-const int itvBuzzOn = 350;     // trajanje u msec piska
-const int itvBuzzOff = 700;    // trajanje u msec vremena izmedju 2 piska
-int buzzLevel = 4;             // jacina zvuka na buzzer-u
-volatile long msPirSignal = 0; // vreme (poslednjeg) PIR signala
-bool firstPirSignal = true;    // prvi PIR signal se javlja odmah po paljenju aparata i smatra se laznim
+const int pinPIR = 2;           // PIR senzor; ako se promeni ova vrednost, obavezno izmeniti PCINT? u sleep() i setup()
+const int pinVolUp = 1;         // taster za pojacavanje zvuka na buzzer-u
+const int pinVolDown = 3;       // taster za smanjivanje zvuka na buzzer-u
+const int pinBuzz = 0;          // buzzer/zvucnik
+const int itvBuzzOn = 350;      // trajanje u msec piska
+const int itvBuzzOff = 700;     // trajanje u msec vremena izmedju 2 piska
+const int itvVolPress = 3000;   // broj ms za koji se pistanje buzzer-a produzava kod pritiska na neki vol taster
+int vol = 4;                    // jacina zvuka na buzzer-u
+bool volUpPress = false;        // taster za pojacavanje zvuka je pritisnut
+bool volDownPress = false;      // taster za smanjivanje zvuka je pritisnut
+volatile long msPirSignal = 0;  // vreme poslednjeg PIR signala
+long msVolPress = -itvVolPress; // vreme poslednjeg pritiska na neki vol taster
+bool firstPirSignal = true;     // prvi PIR signal se javlja odmah po paljenju aparata i smatra se laznim
 
 void setup()
 {
   pinMode(pinPIR, INPUT);
   pinMode(pinVolUp, INPUT_PULLUP);
+  pinMode(pinVolDown, INPUT_PULLUP);
 
   pinMode(pinBuzz, OUTPUT);
   digitalWrite(pinBuzz, false);
@@ -52,15 +58,56 @@ ISR(PCINT0_vect)
   msPirSignal = millis();
 }
 
+// podesavanje jacine zvuka buzzer-a na osnovu pritiska na vol tastere
+void check_vol_press()
+{
+  const int volCoef = 4;       // koeficijent promene jacine zvuka buzzer-a
+  const int minVol = 1;        // najmanji jacina zvuka buzzer-a
+  const int maxVol = 255;      // najveca jacina zvuka buzzer-a
+  const int itvDebounce = 100; // vol button debounce interval
+
+  // da li je neki taster pritisnut
+  if (!digitalRead(pinVolUp) || !digitalRead(pinVolDown))
+  {
+    msVolPress = millis();
+    delay(itvDebounce);
+
+    if (digitalRead(pinVolUp) == volUpPress)
+    {
+      volUpPress = !volUpPress;
+      if (volUpPress)
+        vol *= volCoef;
+
+      if (vol > maxVol)
+        vol = maxVol;
+
+      analogWrite(pinBuzz, vol);
+      //B delay(itvBuzzOn);
+    }
+    if (digitalRead(pinVolDown) == volDownPress)
+    {
+      volDownPress = !volDownPress;
+      if (volDownPress)
+        vol /= volCoef;
+
+      if (vol < minVol)
+        vol = minVol;
+
+      analogWrite(pinBuzz, vol);
+      //B delay(itvBuzzOn);
+    }
+  }
+}
+
 void loop()
 {
+  check_vol_press();
+
   // PIR -> HIGH => detektovan je pokret
-  if (digitalRead(pinPIR) && !firstPirSignal)
+  if ((digitalRead(pinPIR) && !firstPirSignal) || millis() - msVolPress < itvVolPress)
   {
     int itv = (millis() - msPirSignal) % (itvBuzzOn + itvBuzzOff);
-    // analogWrite(pinBuzz, itv < itvBuzzOn ? valPot : 0);
-    int val = digitalRead(pinVolUp);
-    analogWrite(pinBuzz, val ? 40 : 4);
+    analogWrite(pinBuzz, itv < itvBuzzOn ? vol : 0);
     delay(10);
   }
   // PIR -> LOW => nije detektovana kretnja
